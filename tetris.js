221ad7c928de1d6506062581521db29069d353fb
loone96 <andy960925>
2020-03-01 01:54:48 +0900

    tetris mobile

--- tetris.js
+++ tetris.js
@@ -239,25 +239,6 @@ document.addEventListener('keydown', event => {
     }
 });
 
-const colors = [
-    null,
-    '#FF0D72',
-    '#0DC2FF',
-    '#0DFF72',
-    '#F538FF',
-    '#FF8E0D',
-    '#FFE138',
-    '#3877FF',
-];
-
-const arena = createMatrix(12, 20);
-
-const player = {
-    pos: {x: 0, y: 0},
-    matrix: null,
-    score: 0,
-};
-
 function gameplay(){
     playerReset();
     updateScore();
